5167ece398d3dcaea20814e458e73f6a42bd37c1
Dillon Mulroy <dillon.mulroy@formidable.com>
2019-10-28 10:40:32 -0400

    Modal

--- src/App.js
+++ src/App.js
@@ -9,19 +9,28 @@ import {
 import Auth from './Auth';
 import PrivateRoute from './PrivateRoute';
 import TopTracks from './TopTracks';
+import Modal from './Modal';
 
 const App = () => {
   return (
     <Router>
-      <header>
-        <h1>My Top Tracks</h1>
-        <h2>Discover and share your most played Spotify tracks</h2>
-      </header>
-      <Switch>
-        <Route path='/auth' component={Auth} />
-        <PrivateRoute path='/tracks' component={TopTracks} />
-        <Redirect to='/auth' />
-      </Switch>
+      <section className='is-fullheight'>
+        <div className='columns is-desktop is-vcentered has-text-centered'>
+          <div className='column is-half is-offset-3'>
+            <h1 className='title'>My Top Tracks</h1>
+            <h2 className='subtitle'>
+              Discover and share your most played Spotify tracks
+            </h2>
+            <button class='button is-primary'>Get Started</button>
+          </div>
+        </div>
+        {/* <Switch>
+          <Route path='/auth' component={Auth} />
+          <PrivateRoute path='/tracks' component={TopTracks} />
+          <Redirect to='/auth' />
+        </Switch> */}
+      </section>
+      <Modal open />
     </Router>
   );
 };
